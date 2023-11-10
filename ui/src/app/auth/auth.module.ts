3ab837caf9efb74ca4d4f71b373eabc132305912
LiciuStefan <licius9090@gmail.com>
2023-11-10 21:56:23 +0200

    Adding auth service for register and login

--- ui/src/app/auth/auth.module.ts
+++ ui/src/app/auth/auth.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { AuthService } from './services/auth.service';
 
 
 @NgModule({
@@ -18,6 +19,9 @@ import { LoginPageComponent } from './pages/login-page/login-page.component';
   exports: [
     RegisterPageComponent,
     LoginPageComponent
-  ]
+  ],
+  providers: [
+    AuthService
+   ]
 })
 export class AuthModule { }
