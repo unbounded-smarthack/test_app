9588f07bce62e7c28d6212c9d5a53fc8c596fcad
Sabau Eduard <59087888+SabauEduard@users.noreply.github.com>
2023-11-10 18:19:46 +0200

    Add tag details view and categories

--- api/src/blog/serializers.py
+++ api/src/blog/serializers.py
@@ -2,7 +2,7 @@ import re
 
 from rest_framework import serializers
 
-from blog.models import Tag
+from blog.models import Tag, Category
 
 
 class TagSerializer(serializers.ModelSerializer):
@@ -19,3 +19,29 @@ class TagSerializer(serializers.ModelSerializer):
             slug=slug
         )
         return tag
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.slug = re.sub("[^a-z0-9-]", "", instance.name.lower().replace(" ", "-"))
+        instance.save()
+        return instance
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ('id', 'name', 'slug')
+        read_only_fields = ('id', 'slug')
+
+    def create(self, validated_data):
+        name = validated_data['name']
+        slug = re.sub("[^a-z0-9-]", "", name.lower().replace(" ", "-"))
+        category = Category.objects.create(
+            name=name,
+            slug=slug
+        )
+        return category
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.slug = re.sub("[^a-z0-9-]", "", instance.name.lower().replace(" ", "-"))
+        instance.save()
+        return instance
--- api/src/blog/views/categories.py
+++ api/src/blog/views/categories.py
@@ -4,28 +4,28 @@ from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST
 from rest_framework.views import APIView
 
-from blog.models import Tag
-from blog.serializers import TagSerializer
+from blog.models import Category
+from blog.serializers import CategorySerializer
 
 
-class TagListView(APIView):
+class CategoryListView(APIView):
     @extend_schema(
-        summary='Tag List',
-        description="Tag List",
-        request=TagSerializer,
+        summary='Category List',
+        description="Category List",
+        request=CategorySerializer,
         responses={
             200: OpenApiResponse(description='Json Response with the data'),
         }
     )
     def get(self, request):
-        tags = Tag.objects.all()
-        serializer = TagSerializer(tags, many=True)
+        categories = Category.objects.all()
+        serializer = CategorySerializer(categories, many=True)
         return Response(serializer.data, status=HTTP_200_OK)
 
     @extend_schema(
-        summary='Tag Create',
-        description="Tag Create",
-        request=TagSerializer,
+        summary='Category Create',
+        description="Category Create",
+        request=CategorySerializer,
         responses={
             201: OpenApiResponse(description='Json Response with the message'),
             400: OpenApiResponse(description='Json Response with the errors'),
@@ -35,56 +35,53 @@ class TagListView(APIView):
         data = {
             'name': request.data.get('name'),
         }
-        serializer = TagSerializer(data=data)
+        serializer = CategorySerializer(data=data)
         if serializer.is_valid():
             serializer.save()
-            return Response({"message": "Tag successfully added!"}, status=HTTP_201_CREATED)
+            return Response({"message": "Category successfully added!"}, status=HTTP_201_CREATED)
         return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
 
 
-class TagDetailView(APIView):
+class CategoryDetailView(APIView):
     @extend_schema(
-        summary='Tag Detail',
-        description="Tag Detail",
-        request=TagSerializer,
+        summary='Category Detail',
+        description="Category Detail",
+        request=CategorySerializer,
         responses={
             200: OpenApiResponse(description='Json Response with the data'),
         }
     )
     def get(self, request, pk):
-        tag = get_object_or_404(Tag, pk=pk)
-        serializer = TagSerializer(tag)
+        category = get_object_or_404(Category, pk=pk)
+        serializer = CategorySerializer(category)
         return Response(serializer.data, status=HTTP_200_OK)
 
     @extend_schema(
-        summary='Tag Update',
-        description="Tag Update",
-        request=TagSerializer,
+        summary='Category Update',
+        description="Category Update",
+        request=CategorySerializer,
         responses={
             200: OpenApiResponse(description='Json Response with the message'),
             400: OpenApiResponse(description='Json Response with the errors'),
         }
     )
     def put(self, request, pk):
-        tag = get_object_or_404(Tag, pk=pk)
-        data = {
-            'name': request.data.get('name'),
-        }
-        serializer = TagSerializer(tag, data=data)
+        category = get_object_or_404(Category, pk=pk)
+        serializer = CategorySerializer(category, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response({"message": "Tag successfully updated!"}, status=HTTP_200_OK)
+            return Response({"message": "Category successfully updated!"}, status=HTTP_200_OK)
         return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
 
     @extend_schema(
-        summary='Tag Delete',
-        description="Tag Delete",
-        request=TagSerializer,
+        summary='Category Delete',
+        description="Category Delete",
+        request=CategorySerializer,
         responses={
             200: OpenApiResponse(description='Json Response with the message'),
         }
     )
     def delete(self, request, pk):
-        tag = get_object_or_404(Tag, pk=pk)
-        tag.delete()
-        return Response({"message": "Tag successfully deleted!"}, status=HTTP_200_OK)
+        category = get_object_or_404(Category, pk=pk)
+        category.delete()
+        return Response({"message": "Category successfully deleted!"}, status=HTTP_200_OK)
