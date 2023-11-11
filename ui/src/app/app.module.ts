ae1bce4dfd5eac8c6321d3781d3bfd29f888b518
Victor Mura <victormura.s@gmail.com>
2023-11-11 16:49:28 +0200

    Add Flutter

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -4,6 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+<<<<<<< Updated upstream
+=======
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import { HttpClientModule } from '@angular/common/http';
+import {AuthModule} from "@auth0/auth0-angular";
+import {CoreModule} from "./core/core.module";
+>>>>>>> Stashed changes
 
 @NgModule({
   declarations: [
@@ -12,7 +20,25 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
+<<<<<<< Updated upstream
     BrowserAnimationsModule
+=======
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonToggleModule,
+    HttpClientModule,
+
+    AuthModule.forRoot({
+      domain: 'dev-3nepo87s1vxqfzck.us.auth0.com',
+      clientId: 'TEChfij56XrDEOryupXaHL2oH9wnN0mY',
+      authorizationParams: {
+        redirect_uri: window.location.origin
+      }
+    }),
+
+    CoreModule
+>>>>>>> Stashed changes
   ],
   providers: [],
   bootstrap: [AppComponent]
