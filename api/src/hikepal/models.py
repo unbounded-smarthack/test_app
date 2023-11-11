0f9531dc617ca8849bf2e6fb312533b4061894a6
Sabau Eduard <59087888+SabauEduard@users.noreply.github.com>
2023-11-11 17:56:06 +0200

    Add trail and activity models

--- api/src/hikepal/models.py
+++ api/src/hikepal/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Trail(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
+    distance = models.IntegerField()
+    elevation_gain = models.IntegerField()
+    elevation_loss = models.IntegerField()
+    recommended_experience = models.IntegerField()
+    estimated_duration = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Activity(models.Model):
+    distance = models.IntegerField()
+    elevation_gain = models.IntegerField()
+    elevation_loss = models.IntegerField()
+    duration = models.IntegerField()
+    experience_gain = models.IntegerField()
+    user = models.ForeignKey('customusers.CustomUser', related_name='activities', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
