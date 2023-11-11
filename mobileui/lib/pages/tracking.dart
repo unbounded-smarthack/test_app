588971954c6273ca164346896a304717505167af
Victor Mura <victormura.s@gmail.com>
2023-11-11 20:00:35 +0200

    Add traking location functionality

--- mobileui/lib/pages/tracking.dart
+++ mobileui/lib/pages/tracking.dart
@@ -1,20 +1,26 @@
+import 'dart:async';
+import 'dart:isolate';
+
 import 'package:flutter/material.dart';
-import 'package:mobileui/services/geolocation.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:location/location.dart';
 
 class GeolocationRecord {
-  final double latitude;
-  final double longitude;
+  final double? latitude;
+  final double? longitude;
+  final double? altitude;
   final DateTime timestamp;
 
   GeolocationRecord({
     required this.latitude,
     required this.longitude,
+    required this.altitude,
     required this.timestamp,
   });
 }
 
 class TrackingPage extends StatefulWidget {
-  TrackingPage({super.key});
+  const TrackingPage({super.key});
 
   @override
   State<TrackingPage> createState() => _TrackingPageState();
@@ -22,42 +28,105 @@ class TrackingPage extends StatefulWidget {
 
 class _TrackingPageState extends State<TrackingPage> {
   List<GeolocationRecord> geolocationRecords = [];
+  bool isTracking = false;
+  double distance = 0;
+  double elevationGain = 0;
+  double elevationLoss = 0;
 
   // Get Every 1 second geolocation
-
   // Add to geolocationRecords
-  void _incrementCounter() {
+  void start() {
     setState(() {
-      GeolocationService().determinePosition().then((value) {
-        geolocationRecords.add(GeolocationRecord(
+      isTracking = !isTracking;
+    });
+    Timer.periodic(const Duration(seconds: 10), (timer) {
+      if (isTracking) {
+          _getGeolocation();
+      };
+    });
+  }
+
+  void _getGeolocation() {
+    setState(() {
+      Location().getLocation().then((value) {
+        GeolocationRecord currentLocation = GeolocationRecord(
           latitude: value.latitude,
           longitude: value.longitude,
+          altitude: value.altitude,
           timestamp: DateTime.now(),
-        ));
+        );
+        if (geolocationRecords.isEmpty) {
+          geolocationRecords.add(currentLocation);
+          return;
+        }
+        GeolocationRecord previousLocation = geolocationRecords.last;
+        if (previousLocation.latitude != null &&
+            previousLocation.longitude != null &&
+            currentLocation.latitude != null &&
+            currentLocation.longitude != null) {
+          double newDistance = Geolocator.distanceBetween(
+            previousLocation.latitude!,
+            previousLocation.longitude!,
+            currentLocation.latitude!,
+            currentLocation.longitude!,
+          );
+          if (newDistance < 20) return;
+
+          distance += newDistance;
+          geolocationRecords.add(currentLocation);
+        }
+
+        if (previousLocation.altitude == null ||
+            currentLocation.altitude == null) {
+          return;
+        }
+        if (previousLocation.altitude! > currentLocation.altitude!) {
+          elevationLoss += previousLocation.altitude! - currentLocation.altitude!;
+        } else {
+          elevationGain += currentLocation.altitude! - previousLocation.altitude!;
+        }
       });
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Material (
-        child: Column(
-          children: [
-            TextButton(
-              onPressed: _incrementCounter,
-              child: const Text("Get Geolocation"),
-            ),
-            SizedBox(
-              height: 300,
-              child: ListView.builder(
-      itemBuilder: geolocationRecords.isEmpty
-                ? (context, index) => const ListTile(title: Text("No data"))
-                : (context, index) => ListTile(
-                      title: Text(geolocationRecords[index].latitude.toString()),
-                    ),
-    ),
-            ),
-          ],
-        ));
+    return Material(
+          child: Column(
+            children: [
+              TextButton(
+                onPressed: start,
+                child: Text(isTracking ? "Stop" : "Start", style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),),
+              ),
+              Text("Distance: ${distance.round()}", style: const TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),),
+              Text("Elevation Gain: ${elevationGain.round()}", style: const TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),),
+              Text("Elevation Loss: ${elevationLoss.round()}", style: const TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: geolocationRecords.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text(
+                          "Lat: ${geolocationRecords[index].latitude} Long: ${geolocationRecords[index].longitude} Alt: ${geolocationRecords[index].altitude}"),
+                      subtitle: Text(
+                          "Timestamp: ${geolocationRecords[index].timestamp}"),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ));
   }
 }
