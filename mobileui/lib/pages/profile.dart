2f4c67ca42d5a92295127dc5e8788166ee12f41d
LiciuStefan <licius9090@gmail.com>
2023-11-12 11:36:46 +0200

    Adding profile page

--- mobileui/lib/pages/profile.dart
+++ mobileui/lib/pages/profile.dart
@@ -1,4 +1,5 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import '../services/profileService.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({Key? key}) : super(key: key);
@@ -8,12 +9,62 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
-
   @override
   Widget build(BuildContext context) {
-    return const CupertinoPageScaffold(
-      child: Center(
-        child: Text("Profile"),
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const Text(
+            "Profile",
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Expanded(
+            child: FutureBuilder(
+              future: ProfileService().getProfileDetails(),
+              builder: (BuildContext context, AsyncSnapshot<ProfileDetails> snapshot) {
+                if (snapshot.hasData) {
+                  // Display user details
+                  return Column(
+                    children: [
+                      const SizedBox(height: 20),
+                      Container(
+                        height: 100,
+                        width: 100,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.blue, // Add your desired color
+                        ),
+                        child: Icon(
+                          Icons.person,
+                          color: Colors.white,
+                          size: 50,
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      Text("Username: ${snapshot.data!.username}"),
+                      const SizedBox(height: 10),
+                      Text(
+                        "${snapshot.data!.experience} XP",
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 30,
+                        ),
+                      ),
+                    ],
+                  );
+                } else {
+                  // Display a loading indicator
+                  return const CircularProgressIndicator();
+                }
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
--- mobileui/lib/services/profileService.dart
+++ mobileui/lib/services/profileService.dart
@@ -5,9 +5,57 @@ import '../const.dart';
 
 
 class ProfileService {
+Future<ProfileDetails> getProfileDetails() async {
+    return http.get(Uri.parse("${API_URL}/api/user/"),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+          // Authorization
+          'Authorization': 'Token ${TOKEN}'
+        },
+        ).then((value) {
+          print(value);
+          if (value.statusCode == 200) {
+            var data = jsonDecode(value.body);
+            print(data['username']);
+            return ProfileDetails(
+              id: data['id'],
+              username: data['username'],
+              email: data['email'],
+              first_name: data['first_name'],
+              last_name: data['last_name'],
+              experience: data['experience'],
+            );
+          }
+          else {
+            print("Error");
+          }
+          return ProfileDetails(
+            id: 0,
+            username: "",
+            email: "",
+            first_name: "",
+            last_name: "",
+            experience: 0,
+          );
+        });
+  }
 
 }
 
 class ProfileDetails {
+  int? id;
+  String username;
+  String email;
+  String first_name;
+  String last_name;
+  int? experience;
 
+  ProfileDetails({
+    required this.id,
+    required this.username,
+    required this.email,
+    required this.first_name,
+    required this.last_name,
+    this.experience,
+  });
 }
