8299e9609be2e47a323f46a08538c60b8b0e9e99
Sabau Eduard <59087888+SabauEduard@users.noreply.github.com>
2023-11-12 08:06:49 +0200

    Add algorithm to calculate experience

--- api/src/hikepal/models.py
+++ api/src/hikepal/models.py
@@ -1,7 +1,35 @@
 from django.db import models
+from django.db.models import Max
 
-from app.settings import EXPERIENCE_GAIN_WEIGHT
-from hikepal.utils import calculate_experience
+from app.settings import (
+    EXPERIENCE_GAIN_WEIGHT,
+    DISTANCE_WEIGHT,
+    ELEVATION_GAIN_WEIGHT,
+    ELEVATION_LOSS_WEIGHT,
+    DURATION_WEIGHT,
+    MAX_EXPERIENCE_GAIN,
+    SCALE_FACTOR,
+)
+from hikepal.utils import normalize_value
+
+
+def calculate_experience(distance, elevation_gain, elevation_loss, duration):
+    normalized_distance = normalize_value(distance, Trail.calculate_max_distance())
+    normalized_elevation_gain = normalize_value(
+        elevation_gain, Trail.calculate_max_elevation_gain()
+    )
+    normalized_elevation_loss = normalize_value(
+        elevation_loss, Trail.calculate_max_elevation_loss()
+    )
+    normalized_duration = normalize_value(duration, Trail.calculate_max_duration())
+
+    experience = (
+        normalized_distance * DISTANCE_WEIGHT
+        + normalized_elevation_gain * ELEVATION_GAIN_WEIGHT
+        + normalized_elevation_loss * ELEVATION_LOSS_WEIGHT
+        + normalized_duration * DURATION_WEIGHT
+    ) * SCALE_FACTOR
+    return experience
 
 
 class Trail(models.Model):
@@ -12,6 +40,33 @@ class Trail(models.Model):
     elevation_loss = models.IntegerField()
     recommended_experience = models.IntegerField()
     estimated_duration = models.IntegerField()
+
+    @classmethod
+    def calculate_max_distance(cls):
+        return cls.objects.aggregate(Max("distance"))["distance__max"]
+
+    @classmethod
+    def calculate_max_elevation_gain(cls):
+        return cls.objects.aggregate(Max("elevation_gain"))["elevation_gain__max"]
+
+    @classmethod
+    def calculate_max_elevation_loss(cls):
+        return cls.objects.aggregate(Max("elevation_loss"))["elevation_loss__max"]
+
+    @classmethod
+    def calculate_max_duration(cls):
+        return cls.objects.aggregate(Max("estimated_duration"))[
+            "estimated_duration__max"
+        ]
+
+    def save(self, *args, **kwargs):
+        self.recommended_experience = calculate_experience(
+            self.distance,
+            self.elevation_gain,
+            self.elevation_loss,
+            self.estimated_duration,
+        )
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
@@ -29,8 +84,12 @@ class Activity(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def calculate_experience_gain(self):
-        return EXPERIENCE_GAIN_WEIGHT * calculate_experience(
-            self.distance, self.elevation_gain, self.elevation_loss, self.duration
+        return max(
+            EXPERIENCE_GAIN_WEIGHT
+            * calculate_experience(
+                self.distance, self.elevation_gain, self.elevation_loss, self.duration
+            ),
+            MAX_EXPERIENCE_GAIN,
         )
 
     def __str__(self):
